fc9a7024cf1ff39b5ecb403bae7179c44c6b3908
guoxiaofeng <1121553503@qq.com>
2020-06-06 17:52:06 +0800

    设计模式

---
+++ src/main/java/com/gxf/www/design_policy/DesignPolicyApplication.java
@@ -0,0 +1,13 @@
+package com.gxf.www.design_policy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPolicyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPolicyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gxf/www/design_policy/DesignPolicyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxf.www.design_policy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPolicyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
